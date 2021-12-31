e7f84c0948bcb3ddbac753854bb1eebc87285891
Furkan Beydemir <furkanbeydemirr@gmail.com>
2021-12-31 11:50:15 +0300

    basic landing page

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$('.capsulprice').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    doot:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+
+        768: {
+            items:3
+        },
+        1199:{
+            items:4
+        }
+
+
+    }
+})
